b29572ef4a57e84b07d3f2228bcb3ec23ab2f0d7
unknown <riponhembrom804@gmail.com>
2025-08-12 04:12:21 +0600

    added 3,4,5,6 problem

--- problem1.js
+++ problem1.js
@@ -18,3 +18,65 @@ else if(money<=10000){
 else{
     console.log("Chocolate");
 }
+
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 11 ;
+//write your code here
+for(let day=1; day<=lastDay; day++){
+    if(day%3==0){
+          console.log(`${day} - medicine`);
+    }
+    else {
+        console.log(`${day} - rest`);
+    }
+}
+
+
+
+
+
+
+
+/** Problem 04 - (Delete / Store) */
+var fileName= "pdfData.jpg";
+//write your code here
+
+if (
+    fileName.startsWith("#") ||
+    fileName.endsWith(".pdf") ||
+    fileName.endsWith(".docx")
+) {
+    console.log("Store");
+} else {
+    console.log("Delete");
+}
+
+
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
+//write your code here
+var email = student.name + student.roll + "." + student.department + "@ph.ac.bd";
+console.log(email); 
+
+
+student = { name: "monu", roll: 99, department: "eee" };
+email = student.name + student.roll + "." + student.department + "@ph.ac.bd";
+console.log(email); 
+
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+//write your code here
+
+var currentSalary = (startingSalary * Math.pow(1.05, experience)).toFixed(2);
+
+console.log(currentSalary); 
+
+startingSalary = 15000; experience = 3;
+currentSalary = (startingSalary * Math.pow(1.05, experience)).toFixed(2);
+console.log(currentSalary); 
+
+startingSalary = 30000; experience = 40;
+currentSalary = (startingSalary * Math.pow(1.05, experience)).toFixed(2);
+console.log(currentSalary); 
